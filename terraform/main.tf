9fa9e68c6c82ad4b003f27c3554782f1c8b734f2
David Sanchez <d4vsanchez@gmail.com>
2021-12-26 12:01:01 -0500

    Use variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "terraform-test-instance" {
-  ami           = "ami-074d542b2787603f8"
-  instance_type = "t2.micro"
-  tags = {
-    Name        = "getting-started"
-    Environment = "Dev"
+variable "ami_id" {
+  type        = string
+  description = "The AMI ID that we want to use to create the new EC2 instance"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "EC2's instance type"
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    "Name" : "Getting Started",
+    "Environment" : "Dev"
   }
+}
+
+resource "aws_instance" "terraform-test-instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags          = var.tags
 }
