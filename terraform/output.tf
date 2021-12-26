787411565f7073b43eaa1562201b7204dd30aea6
David Sanchez <d4vsanchez@gmail.com>
2021-12-26 18:33:54 -0500

    Create KMS key

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "instance_ip_addr" {
   value = aws_instance.terraform-test-instance.*.public_ip
 }
+
+output "encryption_key_arn" {
+  value = aws_kms_key.encryption_key.arn
+}
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -11,8 +11,21 @@ variable "bucket_tags" {
   type = map(any)
 }
 
+resource "aws_kms_key" "encryption_key" {
+  description = "Encrypt terraform-backend data"
+}
+
 resource "aws_s3_bucket" "terraform-backend" {
   bucket = var.bucket_name
   acl    = var.bucket_acl
   tags   = var.bucket_tags
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = aws_kms_key.encryption_key.arn
+      }
+    }
+  }
 }
