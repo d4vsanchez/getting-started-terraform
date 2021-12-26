b35a482740d4403c5309a5d0f3686bd2921e24f8
David Sanchez <d4vsanchez@gmail.com>
2021-12-26 13:41:44 -0500

    Create security group, support SSH and HTTP

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,27 @@ variable "tags" {
 }
 
 resource "aws_instance" "terraform-test-instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags          = var.tags
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  tags            = var.tags
+  security_groups = [aws_security_group.allow-ssh.name]
+}
+
+resource "aws_security_group" "allow-ssh" {
+  name        = "allow-ssh"
+  description = "Allow SSH connections in the EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Bad practice in this case, gotta improve this
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
